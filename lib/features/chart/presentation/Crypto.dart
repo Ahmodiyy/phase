78565af29791542acc653f0eba201cf5c40e69c8
Ahmodiyy <ahmodolaitan03@gmail.com>
2023-05-13 11:58:20 +0100

    using feature first approch

---
+++ lib/features/chart/presentation/Crypto.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Crypto extends ConsumerWidget {
+  const Crypto({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SafeArea(child: Scaffold());
+  }
+}
